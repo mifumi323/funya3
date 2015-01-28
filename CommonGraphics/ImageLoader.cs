35fa0ab31dd04c003ef68c8a01e7407d52ca6545
mifumi323 <mifumi323@gmail.com>
2015-01-28 19:58:13 +0900

    クラス名変更と画像ローダー

---
+++ CommonGraphics/ImageLoader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MifuminSoft.funya3.Graphics
+{
+    public abstract class ImageLoader
+    {
+        public abstract Image Load(string name);
+    }
+}
